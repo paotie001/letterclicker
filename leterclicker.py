b0b94e54b9ce103e581f17f9991295a5c940a568
paotie001 <phucthai797@gmail.com>
2025-07-20 22:52:31 +0700

    Update leterclicker.py
    hotfix

--- leterclicker.py
+++ leterclicker.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter import simpledialog
 
 point = 0
-clickPower = 1
+CP = 1
 
 # --- Classes ---
 class Perk:
@@ -29,34 +29,34 @@ def check_perk1():
 # --- Functions ---
 def click():
     global point
-    point += clickPower
+    point += CP
     pointLabelPoint.config(text=str(point) + ' coin')
     check_perk1()
 
 def up1():
-    global point, clickPower
+    global point, CP
     if point >= upgrade1_obj.cost:
         point -= upgrade1_obj.cost
-        clickPower += 1
+        CP += 1
         upgrade1_obj.update()
         pointLabelPoint.config(text=str(point) + ' coin')
-        pointLabelPower.config(text=str(clickPower) + ' click power')
+        pointLabelPower.config(text=str(CP) + ' click power')
         upgrade1_button.config(
             text=f"({upgrade1_obj.level}) +1 click power {upgrade1_obj.cost} coin"
         )
 def perk1():
-    global point, clickPower,perk1_obj
+    global point, CP,perk1_obj
     if point >= perk1_obj.cost:
         point -= perk1_obj.cost
-        clickPower *= 1.5
+        CP *= 1.5
         click_me.config(font=('Ariel', 50,"bold"))
         pointLabelPoint.config(text=str(point) + ' coin')
-        pointLabelPower.config(text=str(clickPower) + ' click power')
+        pointLabelPower.config(text=str(CP) + ' click power')
         perk1_button.destroy()
 # --- Windows ---
 game = tk.Tk()
 game.title("Clicker")
-game.geometry("800x450")
+game.geometry("450x800")
 
 player_name = simpledialog.askstring("Player Name", "Enter your name:", parent=game)
 if not player_name:
@@ -69,40 +69,39 @@ pointLabelPoint = tk.Label(
     font=('Ariel', 30))
 pointLabelPower = tk.Label(
     game,
-    text=str(clickPower) + ' click power')
+    text=str(CP) + ' click power')
 
 click_me = tk.Button(
     game,
     text=str(player_name),
     fg=('black'),
-    bg=('black'),
+    bg=('white'),
     font=('Ariel', 50),
     command=click
 )
 
 upgrade1_button = tk.Button(
     game,
-    text=f"({upgrade1_obj.level}) +1 click power {upgrade1_obj.cost} coin",
+    text=f"({upgrade1_obj.level}) +1 CP \n{upgrade1_obj.cost} coin",
     font=('Ariel', 20),
     command=up1,
-    height=1,
-    width=30
+    height=2,
+    width=10
 )
 perk1_button = tk.Button(
     game,
-    text=f"change color x1.5 click power {perk1_obj.cost} coin",
+    text=f"BOLD x1.5 CP \n{perk1_obj.cost} coin",
     font=('Ariel', 20),
     command=perk1,
-    height=1,
-    width=30
+    height=2,
+    width=10
 )
 # --- Layout ---
 pointLabelPoint.pack()
 pointLabelPower.pack()
 click_me.pack()
-upgrade1_button.pack(side='right')
+upgrade1_button.pack(side='left')
 if point>=50:
-    perk1_button.pack(side='left')
+    perk1_button.pack(side='right')
 # --- Mainloop ---
 game.mainloop()
-
