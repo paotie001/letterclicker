981cb3199a3abb30a514889ac54c777e9ba65998
paotie001 <phucthai797@gmail.com>
2025-07-21 08:40:39 +0700

    Update leterclicker.py

--- leterclicker.py
+++ leterclicker.py
@@ -9,50 +9,59 @@ class Perk:
     def __init__(self,type,cost):
         self.type=type
         self.cost=cost
+        self.button=None
+    def command(self):
+        global point, CP
+        if point >= self.cost:
+            point -= self.cost
+            CP *= 1.5
+            click_me.config(font=('Ariel', 50, self.type))
+            pointLabelPoint.config(text=str(point) + ' coin')
+            pointLabelPower.config(text=str(CP) + ' click power')
+            self.button.destroy()
+
+    def checkconp(self):#check condition
+        if point >= self.cost/2 and not self.button.winfo_ismapped():
+            self.button.pack(side='left')
+        elif point < self.cost/2 and self.button.winfo_ismapped():
+            self.button.pack_forget()
 class Upgrade:
     def __init__(self, level, cost):
         self.level = level
         self.cost = cost
-
+        self.button=None
     def update(self):
         self.level += 1
         self.cost = int(self.cost * 1.15) + 1
 
+    def command(self):
+        global point, CP
+        if point >= self.cost:
+            point -= self.cost
+            CP += 1
+            self.update()
+            pointLabelPoint.config(text=str(point) + ' coin')
+            pointLabelPower.config(text=str(CP) + 'CP')
+            if self.button:
+                self.button.config(
+                    text=f"({self.level}) +1 CP {self.cost} coin"
+                )
+    def checkconu(self):#check condition
+        if point >= self.cost/2 and not self.button.winfo_ismapped():
+            self.button.pack(side='right')
+        elif point < self.cost/2 and self.button.winfo_ismapped():
+            self.button.pack_forget()
 # --- Objects ---
-upgrade1_obj = Upgrade(0, 10)
-perk1_obj=Perk('bold',100)
-def check_perk1():
-    if point >= 50 and not perk1_button.winfo_ismapped():
-        perk1_button.pack(side='left')  # Show if not already visible
-    elif point < 50 and perk1_button.winfo_ismapped():
-        perk1_button.pack_forget()
+upgrade1 = Upgrade(0, 10)
+perk1=Perk('bold',100)
+
 # --- Functions ---
 def click():
     global point
     point += CP
     pointLabelPoint.config(text=str(point) + ' coin')
-    check_perk1()
+    perk1.checkconp()
 
-def up1():
-    global point, CP
-    if point >= upgrade1_obj.cost:
-        point -= upgrade1_obj.cost
-        CP += 1
-        upgrade1_obj.update()
-        pointLabelPoint.config(text=str(point) + ' coin')
-        pointLabelPower.config(text=str(CP) + ' click power')
-        upgrade1_button.config(
-            text=f"({upgrade1_obj.level}) +1 click power {upgrade1_obj.cost} coin"
-        )
-def perk1():
-    global point, CP,perk1_obj
-    if point >= perk1_obj.cost:
-        point -= perk1_obj.cost
-        CP *= 1.5
-        click_me.config(font=('Ariel', 50,"bold"))
-        pointLabelPoint.config(text=str(point) + ' coin')
-        pointLabelPower.config(text=str(CP) + ' click power')
-        perk1_button.destroy()
 # --- Windows ---
 game = tk.Tk()
 game.title("Clicker")
@@ -80,19 +89,19 @@ click_me = tk.Button(
     command=click
 )
 
-upgrade1_button = tk.Button(
+upgrade1.button = tk.Button(
     game,
-    text=f"({upgrade1_obj.level}) +1 CP \n{upgrade1_obj.cost} coin",
+    text=f"({upgrade1.level}) +1 CP \n{upgrade1.cost} coin",
     font=('Ariel', 20),
-    command=up1,
+    command= upgrade1.command,
     height=2,
     width=10
 )
-perk1_button = tk.Button(
+perk1.button = tk.Button(
     game,
-    text=f"BOLD x1.5 CP \n{perk1_obj.cost} coin",
+    text=f"BOLD x1.5 CP \n{perk1.cost} coin",
     font=('Ariel', 20),
-    command=perk1,
+    command=perk1.command,
     height=2,
     width=10
 )
@@ -100,8 +109,8 @@ perk1_button = tk.Button(
 pointLabelPoint.pack()
 pointLabelPower.pack()
 click_me.pack()
-upgrade1_button.pack(side='left')
-if point>=50:
-    perk1_button.pack(side='right')
+upgrade1.button.pack(side='left')
+if point!=0: #every other upgrades and perk goes here
+    perk1.button.pack(side='right')
 # --- Mainloop ---
 game.mainloop()
